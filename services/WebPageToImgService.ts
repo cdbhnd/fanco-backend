75632fba05a7bbb43092c23f13ed9f7e9eeb8467
Ognjen Tomic <gliamanti@gmail.com>
2017-04-27 11:46:34 +0200

    FC-40 missing inject

--- services/WebPageToImgService.ts
+++ services/WebPageToImgService.ts
@@ -1,13 +1,18 @@
 import { IWebPageToImgService } from "./IWebPageToImgService";
 import { HtmlPageToImageProvider } from "../providers/pageToImageProvider";
+import { injectable } from "inversify";
 
+@injectable()
 export class WebPageToImgService implements IWebPageToImgService {
     public async getPageImgByUrl(link: string): Promise<string> {
+        console.log("UNISHOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO !!!!!!!!!!!!!!");
+        console.log(link);
         let res = await this.getImageAction(link);
         return JSON.stringify(res);
     }
 
     private async getImageAction(link) {
+        console.log("GET IMAGE ACCTIONNNN ");
         let imageProvider: HtmlPageToImageProvider = new HtmlPageToImageProvider();
         let response = await imageProvider.getImageFromURL(link);
         let jsonResponse: any = JSON.parse(response);
