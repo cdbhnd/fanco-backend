3a1d1099a3fec4524c82b7962f5e232a4818f236
uros pajic <uros pajic>
2017-06-07 08:38:11 +0200

    FC-56 CRUD APIs for IPoll has been implemented

--- web/controllers/PollController.ts
+++ web/controllers/PollController.ts
@@ -1,11 +1,10 @@
-import * as actions from "../../actions/";
+import { ActionBase, ActionContext, GetPolls, CreatePoll, UpdatePoll, DeletePoll } from "../../actions/";
 import { ExceptionTypes } from "../../infrastructure/exceptions/";
 import { JsonController, Param, Body, Get, Post, Put, Delete, Req, Res, HttpCode, UseBefore, UploadedFile } from "routing-controllers";
-import { ActionBase } from "../../actions/";
-import { ActionContext } from "../../actions";
 import { AuthMiddleware } from "../middleware/authMiddleware";
 import * as config from "config";
 import { HttpError } from "../decorators/httpError";
+import { IPoll } from "../../entities/";
 
 @JsonController()
 export class EventController {
@@ -16,7 +15,13 @@ export class EventController {
     @HttpError(403, ExceptionTypes.UserNotAuthorizedException)
     @HttpError(400, ExceptionTypes.ValidationException)
     public async getPolls( @Param("orgId") orgId: string, @Param("userId") userId: string) {
-        return null;
+        let getPollsAction: ActionBase<IPoll[]> = new GetPolls.Action();
+        let actionContext: ActionContext = new ActionContext();
+        actionContext.params = {
+            organization: orgId,
+            userId: userId
+        };
+        return await getPollsAction.run(actionContext);
     }
 
     @Post("/organization/:orgId/polls")
@@ -25,7 +30,14 @@ export class EventController {
     @HttpError(403, ExceptionTypes.UserNotAuthorizedException)
     @HttpError(400, ExceptionTypes.ValidationException)
     public async createPoll( @Param("userId") userId: string, @Param("orgId") orgId: string, @Body() userSubmitedParams: any) {
-        return null;
+        let createPollAction: ActionBase<IPoll> = new CreatePoll.Action();
+        let actionContext: ActionContext = new ActionContext();
+        actionContext.params = {
+            organization: orgId,
+            userId: userId,
+            data: userSubmitedParams
+        };
+        return await createPollAction.run(actionContext);
     }
 
     @Put("/organization/:orgId/polls/:pollId")
@@ -34,7 +46,15 @@ export class EventController {
     @HttpError(403, ExceptionTypes.UserNotAuthorizedException)
     @HttpError(400, ExceptionTypes.ValidationException)
     public async updatePoll( @Param("userId") userId: string, @Param("orgId") orgId: string, @Param("pollId") pollId: string, @Body() userSubmitedParams: any) {
-        return null;
+        let updatePollAction: ActionBase<IPoll> = new UpdatePoll.Action();
+        let actionContext: ActionContext = new ActionContext();
+        actionContext.params = {
+            organization: orgId,
+            userId: userId,
+            pollId: pollId,
+            data: userSubmitedParams
+        };
+        return await updatePollAction.run(actionContext);
     }
 
     @Delete("/organization/:orgId/polls/:pollId")
@@ -43,6 +63,13 @@ export class EventController {
     @HttpError(403, ExceptionTypes.UserNotAuthorizedException)
     @HttpError(400, ExceptionTypes.ValidationException)
     public async deletePoll( @Param("userId") userId: string, @Param("orgId") orgId: string, @Param("pollId") pollId: string) {
-        return null;
+        let deletePollAction: ActionBase<IPoll> = new UpdatePoll.Action();
+        let actionContext: ActionContext = new ActionContext();
+        actionContext.params = {
+            organization: orgId,
+            userId: userId,
+            pollId: pollId
+        };
+        return await deletePollAction.run(actionContext);
     }
 }
